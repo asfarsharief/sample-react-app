ca76df78c840a61e38edfcef9ce6216b87c6b5bc
Asfar Sharief <asfar.sharief@eagleview.com>
2022-03-06 19:11:36 +0530

    live application

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src="https://img.icons8.com/color/96/000000/in-progress--v3.png" alt="logo" />
+        <p>
+          Karkhana Studio Website In Progress. Please stay tuned
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
